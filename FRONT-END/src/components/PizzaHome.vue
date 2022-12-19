9f2bdeeac244127dc92103d5a716e47c352af637
Andreaboldrin97 <boldrinandrea23@gmail.com>
2022-12-19 13:01:16 +0100

    init vue.js

---
+++ FRONT-END/src/components/PizzaHome.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+      <h1>PIZZe</h1>
+  </div>
+</template>
+
+<script>
+//importiamo axios
+import axios from 'axios';
+//costabte url
+const API_URL = "http://localhost:8080/api/1";
+export default {
+  data() {
+    return {
+       pizze : [],
+    };
+  },
+  methods: {
+
+
+  },
+    mounted() {
+     axios.get(API_URL + '/pizza/all')
+          .then(res => {
+     
+         const pizze = res.data;
+         if (pizze == null) return;
+         this.pizze = pizze;
+         console.log(this.pizze);
+    });
+   
+  }
+}
+</script>
+
+<style>
+
+</style>
